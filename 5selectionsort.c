2b7a86578d800237ac6a44305d15f28ef0dbc066
harsh <harshkarkar333@gmail.com>
2023-08-23 11:49:30 +0530

    ary2

---
+++ 5selectionsort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define n 10
+
+int a[n],i,j,temp;
+
+int selectionsort(int a[] )
+{
+    for(i=0; i< n ; i++)
+    {
+        for(j = i+1; j< n ; j++)
+        {
+            if(a[i] > a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    for ( i = 0; i < n; i++)
+    {
+        a[i] = rand()%55 +1;
+    }
+
+    printf("\n Original array alement is : ");
+    for ( i = 0; i < n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+
+    selectionsort( a );
+
+    printf("\n\n Sorted array element is : ");
+    for ( i = 0; i < n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
